1e986a1f26d0e86623071a878093125dc13635bb
VASCOSORO <virtualgraf00@gmail.com>
2024-10-16 11:07:40 -0300

    Update maqui.py

--- maqui.py
+++ maqui.py
@@ -1,26 +1,35 @@
 import streamlit as st
 import pandas as pd
+import requests
+from io import BytesIO
 
-# Cargar los datos desde el archivo Excel
-file_path = 'Temporada 2025 maquinas mini y superbox.xlsx'
-df = pd.read_excel(file_path, sheet_name='Mini y Superbox', skiprows=2)
+# URL del archivo Excel en GitHub
+url = "https://github.com/VASCOSORO/maquineros/raw/main/Catalogo_Limpio_Mini_Superbox.xlsx"
 
-# Limpiar y renombrar las columnas
-df.columns = ['Unnamed', 'Precio', 'Cantidad', 'Bulto_x', 'Descripcion', 'Entrega', 'Octubre', 'Noviembre', 'Diciembre', 'Enero', 'Febrero']
-df = df.drop(columns=['Unnamed'])
+# Descargar el archivo desde GitHub
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.content
+    df = pd.read_excel(BytesIO(data), sheet_name='Mini y Superbox', skiprows=2)
 
-# Agregar las rutas de las imágenes desde el repositorio en GitHub
-base_url = "https://github.com/VASCOSORO/maquineros/raw/main/"
-df['Imagen'] = df['Descripcion'].apply(lambda x: base_url + x.lower().replace(" ", "_") + ".png")
+    # Limpiar y renombrar las columnas
+    df.columns = ['Unnamed', 'Precio', 'Cantidad', 'Bulto_x', 'Descripcion', 'Entrega', 'Octubre', 'Noviembre', 'Diciembre', 'Enero', 'Febrero']
+    df = df.drop(columns=['Unnamed'])
 
-# Configurar la interfaz de Streamlit
-st.title("Catálogo de Promociones")
+    # Agregar las rutas de las imágenes desde el repositorio en GitHub
+    base_url = "https://github.com/VASCOSORO/maquineros/raw/main/"
+    df['Imagen'] = df['Descripcion'].apply(lambda x: base_url + x.lower().replace(" ", "_") + ".png")
 
-# Mostrar los ítems del catálogo con imágenes
-for index, row in df.iterrows():
-    st.image(row['Imagen'], width=150)
-    st.subheader(row['Descripcion'])
-    st.write(f"Precio: ${row['Precio']}")
-    st.write(f"Bulto: {row['Bulto_x']}")
-    st.write(f"Entrega: {row['Entrega']}")
-    st.write("---")
+    # Configurar la interfaz de Streamlit
+    st.title("Catálogo de Promociones")
+
+    # Mostrar los ítems del catálogo con imágenes
+    for index, row in df.iterrows():
+        st.image(row['Imagen'], width=150)
+        st.subheader(row['Descripcion'])
+        st.write(f"Precio: ${row['Precio']}")
+        st.write(f"Bulto: {row['Bulto_x']}")
+        st.write(f"Entrega: {row['Entrega']}")
+        st.write("---")
+else:
+    st.error("No se pudo descargar el archivo Excel. Verifica la URL.")
